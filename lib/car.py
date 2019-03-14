ea78bf6921830e942d9ddcab4608004b6ca87cc8
fangqiyu <fangqiyu666@gmail.com>
2019-03-14 20:01:07 +0800

    新的模型和架构

--- lib/car.py
+++ lib/car.py
@@ -1,137 +1,45 @@
 # -*- coding:UTF-8 -*-
-"""
-@File    : car.py
-@Time    : 2019/3/12 19:14
-@Author  : Blue Keroro
-"""
+
 import pandas as pd
-from lib import initialData
 
 
 class Car(object):
-    def __init__(self, carId, cares):
-        self.__carId = carId
-        self.__cares = cares
-        self.__currentSpeed = 0  # 初始车辆的速度为零
-        self.__drivePath = list()  # 车辆已经行驶及正在行驶的路径
-        if not (carId in cares.getCarIdList()):
-            raise RuntimeError("Invalid param.")
+    def __init__(self, **data):
+        self.id = data['id'] # 车的id
+        self.srcCross = data['from'] # 起点路口的id
+        self.dstCross = data['to'] # 终点路口的id
+        self.maxSpeed = data['speed'] # 最高速度
+        self.planTime = data['planTime'] # 计划出发事件
 
-    def getCarId(self):
-        """
-        获取CarId
-        :return:
-        """
-        return self.__carId
-
-    def getCarFrom(self):
-        """
-        获取其始发地
-        :return:
-        """
-        return self.__cares.getCarFromByCarId(self.__carId)
-
-    def getCarTo(self):
-        """
-        获取其目的地
-        :return:
-        """
-        return self.__cares.getCarToByCarId(self.__carId)
-
-    def getCarLargestSpeed(self):
-        """
-        获取其最高速度
-        :return:
-        """
-        return self.__cares.getCarLargestSpeedByCarId(self.__carId)
-
-    def getCarPlanTime(self):
-        """
-        获取其出发时间
-        :return:
-        """
-        return self.__cares.getCarPlanTimeByCarId(self.__carId)
-
-    def getCarCurrentSpeed(self):
-        """
-        获取车辆当前速度
-        :return:
-        """
-        return self.__currentSpeed
-
-    def setCarCurrentSpeed(self, currentSpeed):
-        """
-        设置车辆当前速度
-        :return:
-        """
-        if currentSpeed > self.getCarLargestSpeed():
-            raise RuntimeError('Invalid param.')
-        self.__currentSpeed = currentSpeed
-
-    def addDrivePath(self, roadId):
-        """
-        每进入一段路径，添加当前路径id
-        :return:
-        """
-        self.__drivePath.append(roadId)
+        self.status = 'start' # 当前状态，可选值：'start' | 'run' | 'arrival'
+        self.route = [] # 路线
+        self.leaveTime = None # 实际出发时间
+        self.currentLocRoad = None # 当前所在的道路
+        self.currentLocLane = None # 当前所在的车道
+        self.currentLocPos = None # 当前所在距离
 
 
-class Cares(object):
-    def __init__(self, dataCar):
-        self.dataCar = dataCar
+def generateCarInstances(configPath):
+    """
+    生成所有车辆的实例
+    """
+    carSet = {}
+    carData = pd.read_csv(configPath + '/car.csv')
+    for index, row in carData.iterrows():
+        data = {
+            'id': str(row['id']),
+            'from': str(row['from']),
+            'to': str(row['to']),
+            'speed': row['speed'],
+            'planTime': row['planTime'],
+        }
+        carSet[data['id']] = Car(**data)
 
-    def getCarIdList(self):
-        """
-        获取全部的carId
-        :return: list类型
-        """
-        return list(self.dataCar['id'])
-
-    def getCarFromByCarId(self, carId):
-        """
-        根据carId获取其始发地
-        :param carId:
-        :return:
-        """
-        return list(self.dataCar[self.dataCar['id'] == carId]['from'])[0]
-
-    def getCarToByCarId(self, carId):
-        """
-        根据carId获取其目的地
-        :param carId:
-        :return:
-        """
-        return list(self.dataCar[self.dataCar['id'] == carId]['to'])[0]
-
-    def getCarLargestSpeedByCarId(self, carId):
-        """
-        根据carId获取其最高速度
-        :param carId:
-        :return:
-        """
-        return list(self.dataCar[self.dataCar['id'] == carId]['speed'])[0]
-
-    def getCarPlanTimeByCarId(self, carId):
-        """
-        根据carId获取其出发时间
-        :param carId:
-        :return:
-        """
-        return list(self.dataCar[self.dataCar['id'] == carId]['planTime'])[0]
+    return carSet
 
 
-if __name__ == "__main__":
-    configPath = "../CodeCraft-2019/config_10"
-    initialData.initial(configPath)
-    dataCar = pd.read_csv(configPath + '/car.csv')
-    caresVar = Cares(dataCar)
-    print(caresVar.getCarIdList())
-    print(caresVar.getCarFromByCarId(10013)
-          , caresVar.getCarToByCarId(10013)
-          , caresVar.getCarLargestSpeedByCarId(10013)
-          , caresVar.getCarPlanTimeByCarId(10013))
-    carVar = Car(12048, caresVar)
-    print(carVar.getCarFrom()
-          , carVar.getCarTo()
-          , carVar.getCarLargestSpeed()
-          , carVar.getCarPlanTime())
+if __name__ == '__main__':
+    configPath = '../CodeCraft-2019/config'
+    cars = generateCarInstances(configPath)
+    print cars['10000'].__dict__
+
---
+++ lib/map.py
@@ -0,0 +1,74 @@
+# -*- coding:UTF-8 -*-
+
+import pandas as pd
+
+
+class Map(object):
+    def __init__(self, configPath):
+        self.configPath = configPath
+        self.crossRelation = {} # 路口的连接关系
+        self.roadRelation = {} # 道路的连接关系
+
+        self.__build()
+
+    def __build(self):
+        """
+        建立路口和道路的连接关系
+        """
+        crossData = pd.read_csv(self.configPath + '/cross.csv')
+        roadData = pd.read_csv(self.configPath + '/road.csv')
+
+        self.crossRelation = {str(i):{} for i in crossData['id']}
+        for index, row in roadData.iterrows():
+            roadId = str(row['id'])
+            src = str(row['from'])
+            dst = str(row['to'])
+            self.crossRelation[src][dst]=roadId+'-1'
+            if row['isDuplex']:
+                self.crossRelation[dst][src]=roadId+'-2'
+
+        for i in self.crossRelation:
+            for j in self.crossRelation[i]:
+                srcRoad = self.crossRelation[i][j]
+                r4 = crossData[crossData['id']==int(j)].iloc[0]
+                r4 = (str(r4['roadId']),str(r4['roadId.1']),str(r4['roadId.2']),str(r4['roadId.3']))
+                srcDirNum = r4.index(srcRoad[:-2])
+                self.roadRelation[srcRoad] = {}
+                for dstRoad in self.crossRelation[j].values():
+                    if dstRoad[:-2] == srcRoad[:-2]:
+                        pass
+                    else:
+                        dstDirNum = r4.index(dstRoad[:-2])
+                        dirValue = dstDirNum - srcDirNum
+                        if dirValue == 2 or dirValue == -2:
+                            self.roadRelation[srcRoad][dstRoad] = 'forward'
+                        if dirValue == 1 or dirValue == -3:
+                            self.roadRelation[srcRoad][dstRoad] = 'left'
+                        if dirValue == -1 or dirValue == 3:
+                            self.roadRelation[srcRoad][dstRoad] = 'right'
+
+    def getNeighborCross(self, crossId):
+        """
+        获取可达的下一个路口
+        """
+        if crossId in self.crossRelation:
+            return self.crossRelation[crossId]
+        else:
+            raise Exception("Cross Id Not Exist")
+
+    def getNeighborRoad(self, roadId):
+        """
+        获取可达的下一个道路
+        """
+        if roadId in self.roadRelation:
+            return self.roadRelation[roadId]
+        else:
+            raise Exception("Road Id Not Exist")
+        
+
+if __name__ == '__main__':
+    configPath = '../CodeCraft-2019/config'
+    trafficMap = Map(configPath)
+    print trafficMap.crossRelation
+    print trafficMap.roadRelation
+

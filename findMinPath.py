5f92cd9be97fe7a0074451e08b361e9c374a3e05
NNNeo <35443207+NNNeo@users.noreply.github.com>
2019-03-16 12:25:52 +0800

    Add files via upload

--- findMinPath.py
+++ findMinPath.py
@@ -1,70 +1,108 @@
 import pandas as pd
 from lib import initialData
-from lib.cross import Cross
 from lib.cross import Crosses
 from lib import mapHelper
-from lib.road import Roades
-from lib.road import Road
+from lib.road import Roads
+
 
 direct = ["up", "right", "down", "left"]
+ret = []
 
-
-def findMinPath(map, crosses, roads):
+def findMinPath(map, crosses, roads, crossId1, crossId2):
     """
     根据地图获取地图上每个路口到其余路口的最短路径
     两次遍历所有点，求各点之间最短路径
     :param map:
     :return:
     """
-    for crossid1 in crosses.getCrossIdList() :
-        for crossid2 in crosses.getCrossIdList():
-            flag = [0] * (len(crosses.getCrossIdList()) + 1)
-            flag[crossid1] = 1
-            min = [1000] #当前最小距离 使用min[0]调用
-            ret = []
-            temp = []
-            if crossid1 == crossid2:
-                continue
-            else:
-                helper(crossid1, crossid2, map, ret, temp, roads, flag, 0,min)
-        print(str(crossid1) + "to" + str(crossid2) + ":")
-        print(ret)
+    crossList = []
+    flag = [0] * len(crosses.getCrossIdList())
+    if crossId1 == crossId2:
+        return
+    else:
+        helper(crossId1, crossId2, map, roads, crossList, flag)
+        print(str(crossId1) + "to" + str(crossId2) + ":")
+        # print(ret)
+        ret.reverse()
+        # print(ret)
+        ans = [ret[0]]
+        curCross = roads.getAnotherCrossIdByRoadId(crossId2, ret[0])
+        i = 1
+        ret.pop(0)
+        ans1 = []
+        flag = [False]
+        dfs(curCross, ret, ans, ans1,roads, crosses, crossId1,flag)
+        print("zhieli")
+        print(ans)
+        ans.reverse()
+        print(ans)
 
 
-def helper(crossid1, crossid2, map, ret, temp,roads,flag,cur,min):
+def dfs(curCross, ret, ans, ans1,roads, crosses, crossId1,flag):
+    if curCross == crossId1:
+        print("zheli")
+        print(ans)
+        flag[0] = True
+        return
+    roundRoad = []
+    if crosses.getUpRoadId(curCross) in ret:
+        roundRoad.append(crosses.getUpRoadId(curCross))
+        ret.remove(crosses.getUpRoadId(curCross))
+    if crosses.getDownRoadId(curCross) in ret:
+        roundRoad.append(crosses.getDownRoadId(curCross))
+        ret.remove(crosses.getDownRoadId(curCross))
+    if crosses.getLeftRoadId(curCross) in ret:
+        roundRoad.append(crosses.getLeftRoadId(curCross))
+        ret.remove(crosses.getLeftRoadId(curCross))
+    if crosses.getRightRoadId(curCross) in ret:
+        roundRoad.append(crosses.getRightRoadId(curCross))
+        ret.remove(crosses.getRightRoadId(curCross))
+    for road in roundRoad:
+        ans.append(road)
+        newCross = roads.getAnotherCrossIdByRoadId(curCross, road)
+        dfs(newCross,ret,ans,ans1,roads,crosses,crossId1,flag)
+        if flag[0] == True:
+            break
+        ans.remove(road)
+
+
+def helper(crossid1, crossid2, map, roads, crosslist, flag):
     """
     回溯算法的辅助函数, 从crossid1出发至crossId2 找一条路
     :param crossId1,出发点
            crossId2,终点
            map, maphelper对象
-           ret, 存储最短路径
-           temp，存储路径
            roads，roads对象
-           flag，记录当前点是否被走过
-           cur, 当前距离
-           min, 当前最小距离
+           crosslist, 存储当前路口集合
+           flag， 标记是否访问
     :return:
     """
-    if(crossid1 == crossid2 & cur < min[0]):
-        t = temp
-        min[0] = cur
-        ret.pop() # 清空ret
-        ret.append(t)
+    crosslist.append(crossid1)
+    flag[crossid1] = 1
+    if crossid2 in crosslist:
         return
-    for dir in direct:
-        newRoadId = map.getRoadIdByDirection(crossid1,dir)
-        if newRoadId != -1:
-            newCrossId = roads.getAnotherCrossIdByRoadId(crossid1, newRoadId)
-            if flag[newCrossId] == 0:
-                 temp.append(newRoadId)
-                 flag[newCrossId] = 1
-                 cur += roads.getRoadLengthByRoadId(newRoadId)
-                 if cur > min[0] : break  # 当前距离超过当前最小值，剪枝
-                 helper(newCrossId,crossid2,map,ret,temp,roads,flag,cur,min)
-                 temp.pop()
-                 flag[newCrossId] = 0
-            else : continue
-        else : continue
+    nextCross = 0
+    nextRoad = 0
+    min = 1000
+    for cross in crosslist:
+        for dir in direct:
+            newRoadId = map.getRoadIdByDirection(cross, dir)
+            if newRoadId == -1:
+                continue
+            newCross = roads.getAnotherCrossIdByRoadId(cross, newRoadId)
+            if newCross == crossid2:
+                ret.append(newRoadId)
+                return
+            elif flag[newCross] == 1:
+                continue
+            else :
+                if roads.getRoadLengthByRoadId(newRoadId) < min:
+                    min = roads.getRoadLengthByRoadId(newRoadId)
+                    nextCross = newCross
+                    nextRoad = newRoadId
+            # print("newRoadId in " + dir + " is " + str(newRoadId))
+    ret.append(nextRoad)
+    helper(nextCross,crossid2,map,roads,crosslist,flag)
     return
 
 
@@ -75,5 +113,5 @@ if __name__ == '__main__':
     dataRoad = pd.read_csv(configPath + '/road.csv')
     mapHelperVar = mapHelper.MapHelper(dataCross, dataRoad)
     crossesVar = Crosses(dataCross)
-    roadsVar = Roades(dataRoad)
-    findMinPath(mapHelperVar, crossesVar, roadsVar)
+    roadsVar = Roads(dataRoad)
+    findMinPath(mapHelperVar, crossesVar, roadsVar, 1, 34)

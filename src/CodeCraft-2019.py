6ba2b424ac4196f642934e1a2d1f6f8986eeb4ac
bluekeroro <627887301@qq.com>
2019-03-24 21:20:39 +0800

    update 增加判断，上传到平台上时可自动关闭print

--- src/CodeCraft-2019.py
+++ src/CodeCraft-2019.py
@@ -43,25 +43,24 @@ def planTimeWeighted(car):
 
 def main():
     starttime = datetime.datetime.now()
-    # if len(sys.argv) != 5:
-    #     # logging.info('please input args: car_path, road_path, cross_path, answerPath')
-    #     exit(1)
-    # car_path = sys.argv[1]
-    # road_path = sys.argv[2]
-    # cross_path = sys.argv[3]
-    # answer_path = sys.argv[4]
+    if len(sys.argv) == 5:
+        MyLogger.setEnable(False)
+        car_path = sys.argv[1]
+        road_path = sys.argv[2]
+        cross_path = sys.argv[3]
+        answer_path = sys.argv[4]
+    else:
+        car_path = '../config/car.txt'
+        road_path = '../config/road.txt'
+        cross_path = '../config/cross.txt'
+        answer_path = '../config/answer.txt'
+
     # configPath = ''
     # for i in range(len(car_path) - 1, -1, -1):
     #     if car_path[i] == '/':
     #         configPath = car_path[0:i]
-    #
     # MyLogger.print(car_path, road_path, cross_path, answer_path)
     # MyLogger.print(configPath)
-
-    car_path = '../config/car.txt'
-    road_path = '../config/road.txt'
-    cross_path = '../config/cross.txt'
-    answer_path = '../config/answer.txt'
 
     initialData.initial(car_path, cross_path, road_path)
     dataCar = pd.read_csv(changeTXTpathToCSV(car_path))

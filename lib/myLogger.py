4014a9bd9123d263fb030d284a3709faaf6aa3eb
bluekeroro <627887301@qq.com>
2019-03-25 20:52:43 +0800

    新开release_3.25分支，同步fqy代码

---
+++ lib/myLogger.py
@@ -0,0 +1,31 @@
+# -*- coding:UTF-8 -*-
+"""
+@File    : myLogger.py
+@Time    : 2019/3/21 14:49
+@Author  : Blue Keroro
+"""
+
+
+class MyLogger(object):
+    """
+    创建统一的打印方法
+    因为最后提交时如果含有print方法可能会引起运行失败
+    """
+    enable = True
+
+    @classmethod
+    def print(cls, *args):
+        if cls.enable:
+            print(*args)
+
+    @classmethod
+    def setEnable(cls, enable):
+        cls.enable = enable
+
+
+if __name__ == "__main__":
+    MyLogger.print("hhhh", "jkklll1")
+    MyLogger.setEnable(False)
+    MyLogger.print("hhhh", "jkklll2")
+    MyLogger.setEnable(True)
+    MyLogger.print("hhhh", "jkklll3")
--- src/CodeCraft-2019.py
+++ src/CodeCraft-2019.py
@@ -2,6 +2,8 @@
 import sys
 import os
 
+from lib.myLogger import MyLogger
+
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)

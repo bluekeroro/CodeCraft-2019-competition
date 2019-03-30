4a173add0598b9e4eae5ba65f046b59402c2ac9b
bluekeroro <627887301@qq.com>
2019-03-30 12:11:12 +0800

    update 5*7的参数矩阵

--- src/CodeCraft-2019.py
+++ src/CodeCraft-2019.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
+
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
@@ -11,6 +12,7 @@ from lib.road import generateRoadInstances
 from lib.map import Map
 from lib.shortestpath import getShortestPath, countTurning
 from lib.myLogger import MyLogger
+
 
 def main():
     if len(sys.argv) == 5:
@@ -63,42 +65,50 @@ def main():
     carList = sorted(carList, key=lambda x: (x[1]), reverse=True)  # 速度
 
     # 按速度分批次
-    s8carList = [term for term in carList if term[1] >= 8]
-    s6carList = [term for term in carList if 4 < term[1] <= 6]
-    s4carList = [term for term in carList if 2 < term[1] <= 4]
-    s2carList = [term for term in carList if term[1] <= 2]
+    s16carList = [term for term in carList if term[1] == 16]
+    s14carList = [term for term in carList if term[1] == 14]
+    s12carList = [term for term in carList if term[1] == 12]
+    s10carList = [term for term in carList if term[1] == 10]
+    s8carList = [term for term in carList if term[1] == 8]
+    s6carList = [term for term in carList if term[1] == 6]
+    s4carList = [term for term in carList if term[1] == 4]
 
     # 参数矩阵
     param = [
-        # turning 0     1     2    <=3
-        [0.02, 0.02, 0.02, 0.3],  # speed >= 8
-        [0.02, 0.02, 0.04, 0.3],  # 4 <= speed <= 6
-        [0.03, 0.04, 0.04, 0.3],  # 2 <= speed <= 4
-        [0.03, 0.04, 0.04, 0.3]  # speed <= 2
+        # turning 1     2     3     4    5
+        [0.01, 0.01, 0.01, 0.4, 0.4],  # speed == 16
+        [0.01, 0.01, 0.01, 0.4, 0.4],  # speed == 14
+        [0.01, 0.01, 0.01, 0.4, 0.4],  # speed == 12
+        [0.05, 0.05, 0.05, 0.4, 0.4],  # speed == 10
+        [0.1, 0.1, 0.1, 0.4, 0.4],  # speed == 8
+        [0.3, 0.3, 0.3, 0.4, 0.4],  # speed == 6
+        [0.3, 0.3, 0.3, 0.4, 0.4],  # speed == 4
     ]
 
     cnt = 0
-    for i, carList in enumerate([s8carList, s6carList, s4carList, s2carList]):
+    for i, carList in enumerate([s16carList, s14carList, s12carList, s10carList, s8carList, s6carList, s4carList]):
         for term in carList:
-            if term[3] == 0:
-                cnt += param[i][0]
             if term[3] == 1:
-                cnt += param[i][1]
+                cnt += param[i][0]
             if term[3] == 2:
+                cnt += param[i][1]
+            if term[3] == 3:
                 cnt += param[i][2]
-            if term[3] >= 3:
+            if term[3] == 4:
                 cnt += param[i][3]
+            if term[3] >= 5:
+                cnt += param[i][4]
             thisCar = cars[term[0]]
             thisCar.leaveTime = thisCar.planTime + int(cnt)
 
     MyLogger.print(cnt)
 
     # 生成输出文件
-    with open(answer_path, 'w') as file:
-        for carId in cars:
-            thisCar = cars[carId]
-            answer = '(' + ','.join([thisCar.id, str(thisCar.leaveTime), ','.join(thisCar.route)]) + ')'
-            file.write(answer + '\n')
+    file = open(answer_path, 'w')
+    for carId in cars:
+        thisCar = cars[carId]
+        answer = '(' + ','.join([thisCar.id, str(thisCar.leaveTime), ','.join(thisCar.route)]) + ')'
+        file.write(answer + '\n')
 
 
 if __name__ == "__main__":
